bd3fa225612ba96c08474ab286e56fa4e3747bb7
Vsevolod <micromacropunk@gmail.com>
2024-12-30 21:58:22 +0300

    Формат спрайтов приведён к размеру 32 x 32 пикселей

--- main.py
+++ main.py
@@ -23,15 +23,20 @@ def load_image(name, colorkey=None):
 
 
 class Board:
-    def __init__(self, width, height, screen):
+    def __init__(self, width, height, screen, screen_width, screen_height):
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
         self.left = 10
         self.top = 10
-        self.cell_size = 30
+        self.cell_size = 32
         self.mas = [[0] * width for _ in range(height)]
+
         self.grass_image = load_image("grass_image.png")
+        self.border_image = load_image("border_image.png")
+        self.mage_image = load_image("mage_texture1.png")
+
+        self.where_x, self.where_y = 62, 90
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -43,11 +48,15 @@ class Board:
         cs = self.cell_size
         for y in range(self.height):
             for x in range(self.width):
-                if self.mas[y][x] == 1:
+                if (0 <= x <= 1 or 0 <= y <= 1 or self.width - 2 <= x <= self.width - 1 or
+                        self.height - 2 <= y <= self.height - 1):
+                    pygame.draw.rect(screen, pygame.Color('white'), (x * cs + self.left, y * cs + self.top, cs, cs))
+                    screen.blit(self.border_image, (x * cs + self.left, y * cs + self.top))
+                else:
                     pygame.draw.rect(screen, pygame.Color('white'), (x * cs + self.left, y * cs + self.top, cs, cs))
                     screen.blit(self.grass_image, (x * cs + self.left, y * cs + self.top))
-                else:
-                    pygame.draw.rect(screen, pygame.Color('white'), (x * cs + self.left, y * cs + self.top, cs, cs), 1)
+
+        screen.blit(self.mage_image, (self.where_x, self.where_y))
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -95,17 +104,20 @@ class Board:
 def main():
     pygame.init()
     running = True
+    xr = xl = yu = yd = False
+    #  test_hero = load_image("mage_texture.png")
 
-    width = 5
-    height = 7
-    left = 100
-    top = 100
-    cell_size = 50
+    width = 53
+    height = 25
+    left = 62
+    top = 90
+    cell_size = 32
 
-    size = width_ww, height_ww = 450, 550
+    size = width_ww, height_ww = 1820, 980
+
     screen = pygame.display.set_mode(size)
 
-    board = Board(width, height, screen)
+    board = Board(width, height, screen, width_ww, height_ww)
     board.set_view(left, top, cell_size)
 
     pygame.display.set_caption('Лес. Нечисть. Русский рок.')
@@ -114,12 +126,42 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                board.get_click(event.pos)
-                board.update(event.pos)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    yu = True
+                if event.key == pygame.K_DOWN:
+                    yd = True
+                if event.key == pygame.K_RIGHT:
+                    xr = True
+                if event.key == pygame.K_LEFT:
+                    xl = True
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    yu = False
+                if event.key == pygame.K_DOWN:
+                    yd = False
+                if event.key == pygame.K_RIGHT:
+                    xr = False
+                if event.key == pygame.K_LEFT:
+                    xl = False
+
+        if xr:
+            board.where_x += 32
+            xr = False
+        elif xl:
+            board.where_x -= 32
+            xl = False
+        elif yu:
+            board.where_y -= 32
+            yu = False
+        elif yd:
+            board.where_y += 32
+            yd = False
 
         screen.fill((0, 0, 0))
         board.render(screen)
+
         pygame.display.flip()
 
 
